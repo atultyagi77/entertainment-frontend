23d638d035d4f957bb37eacd39fecb46dcbf737c
Atul Kumar <tyagiatul23799@gmail.com>
2024-07-16 15:19:13 +0530

    Fix Login

--- src/Pages/loginSingupPages/Login.jsx
+++ src/Pages/loginSingupPages/Login.jsx
@@ -66,12 +66,10 @@ const Login = () => {
         toast.success("Login Successfully");
         // Redirect to home page after successful login
         navigate("/");
-      } else {
-        toast.error("Login failed, Try Again");
       }
       // Hide spinner after API call completes
       setLoading(false);
-    }, 2000);
+    }, 800);
   };
 
   const handleFocus = (fieldName) => {
